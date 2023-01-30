739709479b94d037e86f83800b4cf9c5077b38f4
mikigroup <mikigroup@gmail.com>
2023-01-30 19:22:02 +0100

    1

--- src/routes/supabaseClient.js
+++ src/routes/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-/* import  { updateUser } from '@supabase/supabase-js'; */
+import  { updateUser } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://orgshebezwfizhmlmeum.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yZ3NoZWJlendmaXpobWxtZXVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTg2MDMzNjMsImV4cCI6MTk3NDE3OTM2M30.0LA1TPH2v93s10ChjJiX6iTX4LSXMsWOe3MTTxb5_74'
