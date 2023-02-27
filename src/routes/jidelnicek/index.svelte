a721068fe3aa64c772ea13fc6c5a1b6545367df7
mikigroup <39709130+mikigroup@users.noreply.github.com>
2023-02-27 10:32:30 +0100

    omg - nejde odradkova

--- src/routes/jidelnicek/index.svelte
+++ src/routes/jidelnicek/index.svelte
@@ -283,7 +283,7 @@
 													</div>
 													<div class="p-5 text-lg my-3 border rounded-lg shadow-md">
 														<p class="pb-1 underline-offset-8 underline ">{menu.title}</p>
-														<span style="white-space: pre-line"><p>{menu.description}</p></span>													
+														<span style="white-space: pre-line">{menu.description}</span>													
 													</div>
 													<hr class="px-5" />
 													<div class="flex justify-end basis-4 pt-2">
