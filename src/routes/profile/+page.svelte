ffb768a78daa35cadcdbf905c32b954963f50d16
mikigroup <mikigroup@gmail.com>
2023-04-15 14:49:02 +0200

    1

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -6,78 +6,9 @@
 	import { page } from "$app/stores";
 
 	export let session: AuthSession
-
-	/* let loading = false;
-  let username: string | null = null;
-  let website: string | null = null;
-  onMount(() => {
-    getProfile();
-  });
-
-  const getProfile = async () => {
-    try {
-      loading = true;
-
-      const { user } = session;
-      const { data, error, status } = await supabaseClient
-        .from("profiles")
-        .select(`username, website`)
-        .eq("id", user.id)
-        .single();
-      if (data) {
-        username = data.username;
-        website = data.website;
-        avatarUrl = data.avatar_url;
-      }
-
-      if (error && status !== 406) throw error;
-    } catch (error) {
-      if (error instanceof Error) {
-        alert(error.message);
-      }
-    } finally {
-      loading = false;
-    }
-  }; */
-
-	/* 
-	const session = supabase.auth.session();
-	supabase.auth.onAuthStateChange((_, session) => {
-		user.set(session.user);
-	}); */
-
-	/* let loading = true
-  let user_name = null
-  let website = null
-  let avatar_url = null
-  let email = $user.email;
-
-  function getProfile(node) {
-  try {
-    loading = true;
-    const user = supabase.auth.user();
-
-    supabase
-      .from("profiles")
-      .select(`user_name, website, avatar_url`)
-      .eq("id", user?.id)
-      .single()
-      .then(({ data, error, status }) => {
-        if (data) {
-          user_name = data.user_name;
-          website = data.website;
-          avatar_url = data.avatar_url;
-        }
-        if (error && status !== 406) throw error;
-      });
-    } catch (error) {
-      alert(error.message);
-    } finally {
-      loading = false;
-    }
-  } */
-
-	let loading = false
+  
+  
+	/* let loading = false
 	let username: string | null = null
 	let website: string | null = null
 	let avatarUrl: string | null = null
@@ -156,49 +87,19 @@
 		} finally {
 			loading = false
 		}
-	}
+	} */
 
-	/* async function getProfile() {
-    try {
-      loading = true;
-      /* const user = supabaseClient.auth.user();
-
-      let { data, error, status } = await supabaseClient
-        .from("profile")
-        .select(
-          `first_name, last_name, telephone, company_name, street, street_number, city, ico, dic, company`
-        )
-        .eq("id", $session.id)
-        .single();
-
-      if (error && status !== 406) throw error;
-
-      if (data) {
-        first_name = data.first_name;
-        last_name = data.last_name;
-        telephone = data.telephone;
-        company_name = data.company_name;
-        street = data.street;
-        street_number = data.street_number;
-        ico = data.ico;
-        dic = data.dic;
-        company = data.company;
-        city = data.city;
-      }
-    } catch (error) {
-      alert(error.message);
-    } finally {
-      loading = false;
-    }
-  }  */
 </script>
 <svelte:head>
 	<title>Šťastné srdce - Účet</title>
 	<meta name="description" content="Účet" />
 </svelte:head>
+
 {#if !$page.data.session}
-	NEVIM
+	<p class="pt-10 mt-10">Přihlaš se</p>
 {:else}
 	<Account session={$page.data.session} />
 {/if}
+
+
 
