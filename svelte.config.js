171c16ef4f7502a8b96c400968fe5b8385e4dca3
mikigroup <39709130+mikigroup@users.noreply.github.com>
2024-02-28 10:07:24 +0100

    1

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +10,6 @@ const config = {
 	kit: {
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
 };
 
 export default config;
