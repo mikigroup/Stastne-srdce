f3be49e35700aebb909e9f813d010a497725e371
mikigroup <mikigroup@gmail.com>
2024-05-07 23:20:06 +0200

    1

--- src/routes/kosik/+page.svelte
+++ src/routes/kosik/+page.svelte
@@ -1,54 +1,3 @@
-<!-- <script lang="ts">
-/* 	import Kosik from "./Kosik.svelte"
-	import { page } from "$app/stores"
-	import client from "../../lib/sanityClient"
- */
-	// export let data;
-	// console.log(data.orders.orderNumber);
-
-	/* const fetchLatestOrderId = async () => {
-  try {
-    // Fetch the schema for the "order" type
-    const orders = await client.fetch("*[_type == "order"]{_id}[0]");
-    if (orders && orders._id) {
-      // Extract the _id field
-      const lastOrderId = orders._id;
-      // You can use the lastOrderId as needed
-      console.log("Last order ID:", lastOrderId);
-    }
-  } catch (error) {
-    console.error("Error fetching latest order ID:", error);
-  }
-};
-	console.log(data);
-		console.log(orders); */
-</script> -->
-<!-- 
-<svelte:head>
-	<title>Šťastné srdce - Košík</title>
-	<meta name="description" content="Košík" />
-</svelte:head>
-
-{#if !$page.data.session}
-	<main>
-		<section>
-			<div
-				class="max-w-screen-lg px-4 py-16 mx-auto mt-20 mb-10 text-center rounded-lg bg-stone-100 footer_fix"
-			>
-				<div class="flex">
-					<a href="/login"
-						class="w-full px-4 py-2 text-center text-white transition duration-200 ease-in bg-green-600 rounded-lg shadow-md w btn btn-success hover:bg-green-700 focus:ring-green-500 f ocus:ring-offset-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 s-H-c8uVOHPvWG">Přihlaš se
-					</a>
-				</div>
-			</div>
-		</section>
-	</main>
-{:else}
-	<Kosik session={$page.data.session} />
-{/if}
- -->
-
-
 <script lang="ts">
 	import CartItemsStore from "../Stores/stores";
 	import { get } from "svelte/store";
@@ -66,10 +15,17 @@
 	$: cartItems = $CartItemsStore;
 
 	function removeItem(menuid) {
-		CartItemsStore.update((currentCartItems) => {
-			return currentCartItems.filter((cartItem) => cartItem._id != menuid)
-		})
-	};
+   CartItemsStore.update((currentCartItems) => {
+      return currentCartItems.filter((cartItem) => cartItem._id != menuid);
+    });
+  }
+	
+  $: {$CartItemsStore.forEach((item) => {
+      if (item.quantity === 0) {
+        removeItem(item._id);
+      }
+    });
+  }
 
 $: totalPrice = $CartItemsStore.reduce((sum, item) => sum + item.price * item.quantity, 0);
 $: totalPieces = $CartItemsStore.reduce((sum, item) => sum + item.quantity, 0);
