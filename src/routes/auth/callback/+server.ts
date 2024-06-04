35080807f03fdef097c42f125ce37dc5bc8d3199
mikigroup <39709130+mikigroup@users.noreply.github.com>
2024-06-04 11:06:05 +0200

    1

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,4 +1,4 @@
-import { redirect, type RequestHandler } from "@sveltejs/kit";
+/* import { redirect, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
   const code = url.searchParams.get('code') as string;
@@ -12,4 +12,50 @@ export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
   }
   
   throw redirect(303, '/');
+};
+ */
+
+import type { EmailOtpType } from '@supabase/supabase-js';
+import { redirect } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
+  const token_hash = url.searchParams.get('token_hash');
+  const type = url.searchParams.get('type') as EmailOtpType | null;
+  const next = url.searchParams.get('next') ?? '/';
+
+  const redirectTo = new URL(url);
+  redirectTo.pathname = next;
+  redirectTo.searchParams.delete('token_hash');
+  redirectTo.searchParams.delete('type');
+
+  if (!token_hash || !type) {
+    redirectTo.pathname = '/auth/error';
+    redirectTo.searchParams.append('error', 'missing_token_or_type');
+    return redirect(303, redirectTo);
+  }
+
+  const { data, error } = await supabase.auth.verifyOtp({ type, token_hash });
+
+  if (error) {
+    redirectTo.pathname = '/auth/error';
+    redirectTo.searchParams.append('error', error.message);
+    return redirect(303, redirectTo);
+  }
+
+  redirectTo.searchParams.delete('next');
+
+  if (type === 'signup') {
+    redirectTo.pathname = '/';
+    redirectTo.searchParams.append('success', 'signup');
+  } else if (type === 'recovery') {
+    redirectTo.pathname = '/reset';
+    redirectTo.searchParams.append('token', token_hash);
+  } else {
+    redirectTo.pathname = '/auth/error';
+    redirectTo.searchParams.append('error', 'invalid_type');
+    return redirect(303, redirectTo);
+  }
+
+  return redirect(303, redirectTo);
 };
