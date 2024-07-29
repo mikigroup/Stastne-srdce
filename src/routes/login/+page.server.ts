424fee618f3c6d14dc590533fa63af0afd1b03f2
mikigroup <39709130+mikigroup@users.noreply.github.com>
2024-07-29 19:42:37 +0200

    1

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,24 +1,32 @@
 import { fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
-import { type Provider } from "@supabase/supabase-js";
 
 export const actions: Actions = {
-  handleLogin: async ({ request, locals: { supabase }, url }) => {
-    const formData = await request.formData();
-    // const provider = url.searchParams.get("provider") as Provider | undefined;
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    
-    const { error } = await supabase.auth.signInWithPassword({ email, password });
-    if (error) {
-      console.error(error);
-      return fail(400, { message: { success: false, display: "Neplatné přihlašovací údaje. Zkontrolujte prosím e-mail a heslo." } });
-    } else {
-      throw redirect(303, "/jidelnicek");
-    }
-  }
+	handleLogin: async ({ request, locals: { supabase }, url }) => {
+		const formData = await request.formData();
+		// const provider = url.searchParams.get("provider") as Provider | undefined;
+		const email = formData.get("email") as string;
+		const password = formData.get("password") as string;
 
-/*   handleGoogleLogin: async ({ request, locals: { supabase }, url }) => {
+		const { error } = await supabase.auth.signInWithPassword({
+			email,
+			password
+		});
+		if (error) {
+			console.error(error);
+			return fail(400, {
+				message: {
+					success: false,
+					display:
+						"Neplatné přihlašovací údaje. Zkontrolujte prosím e-mail a heslo."
+				}
+			});
+		} else {
+			throw redirect(303, "/jidelnicek");
+		}
+	}
+
+	/*   handleGoogleLogin: async ({ request, locals: { supabase }, url }) => {
     const provider = url.searchParams.get("provider") as Provider | undefined;
 
     
