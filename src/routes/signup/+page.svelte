a3ac8d54b563fd541a464ef3ecfe7be63f84a94f
mikigroup <mikigroup@gmail.com>
2025-01-07 15:18:55 +0100

    1

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -7,6 +7,10 @@
 	$: ({ session, supabase, user } = data);
 
 	let loading = false;
+	let allergies = "no";
+	let allergiesDescription = "";
+	let deliveryMethod = "";
+	let paymentMethod = "";
 
 	async function signInWithGoogle() {
 		loading = true;
@@ -34,9 +38,9 @@
 </svelte:head>
 
 <section>
-	<div class="pt-20 footer_fix">
+	<div class="footer_fix">
 		<div
-			class="flex flex-col max-w-md px-4 pb-2 mx-auto mt-20 bg-white rounded-lg shadow pt-7 sm:px-6 md:px-8 lg:px-10">
+			class="flex flex-col max-w-md px-4 pb-2 mx-auto bg-white rounded-lg shadow pt-7 sm:px-6 md:px-8 lg:px-10">
 			<div
 				class="self-center mb-2 text-3xl font-light text-gray-800 sm:text-2xl">
 				Vytvoření nového účtu
@@ -75,7 +79,7 @@
 								type="email"
 								id="email"
 								name="email"
-								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-aceholder-gray-400 focus:outline-none focus:border-green-600"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
 								pattern="[^@]+@[^\.]+\..+"
 								placeholder="Email"
 								required />
@@ -103,7 +107,7 @@
 								type="password"
 								id="password"
 								name="password"
-								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-aceholder-gray-400 focus:outline-none focus:border-green-600"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
 								placeholder="Heslo (min 6 znaků)"
 								minlength="6"
 								required />
@@ -130,13 +134,226 @@
 								value={form?.repassword ?? ""}
 								type="password"
 								id="repassword"
-								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-aceholder-gray-400 focus:outline-none focus:border-green-600"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
 								name="repassword"
 								placeholder="Potvrzení hesla (napiš stejné heslo)"
 								minlength="6"
 								required />
 						</div>
 					</div>
+					<!--Nové Prvky-->
+
+					<div class="flex flex-col mb-2">
+						<div class="relative flex">
+							<input
+								value={form?.name ?? ""}
+								type="text"
+								id="name"
+								name="name"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
+								placeholder="Jméno"
+								required />
+						</div>
+					</div>
+					<div class="flex flex-col mb-2">
+						<div class="relative flex">
+							<input
+								value={form?.surname ?? ""}
+								type="text"
+								id="surname"
+								name="surname"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
+								placeholder="Příjmení"
+								required />
+						</div>
+					</div>
+					<!--Dodací adresa-->
+					<h3>
+						Dodací adresa
+					</h3>
+					<div class="flex flex-col mb-2">
+						<div class="relative flex">
+							<input
+								value={form?.street ?? ""}
+								type="text"
+								id="street"
+								name="street"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
+								placeholder="Ulice"
+								required
+							/>
+						</div>
+					</div>
+					<div class="flex flex-col mb-2">
+						<div class="relative flex">
+							<input
+								value={form?.street_number ?? ""}
+								type="text"
+								id="street_number"
+								name="street_number"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
+								placeholder="Číslo popisné"
+								required
+							/>
+						</div>
+					</div>
+					<div class="flex flex-col mb-2">
+						<div class="relative flex">
+							<input
+								value={form?.city ?? ""}
+								type="text"
+								id="city"
+								name="city"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
+								placeholder="Město"
+								required
+							/>
+						</div>
+					</div>
+					<div class="flex flex-col mb-2">
+						<div class="relative flex">
+							<input
+								value={form?.zip ?? ""}
+								type="text"
+								id="zip"
+								name="zip"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
+								placeholder="Směrovací číslo"
+								required
+							/>
+						</div>
+					</div>
+					<div class="flex flex-col mb-2">
+						<div class="relative flex">
+							<input
+								value={form?.telephone ?? ""}
+								type="tel"
+								id="telephone"
+								name="telephone"
+								class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
+								placeholder="Telefon"
+								required
+							/>
+						</div>
+					</div>
+					<!-- Alergie -->
+					<div class="mt-6 mb-4">
+						<h3 class="text-lg font-medium mb-2">Alergie</h3>
+						<div class="flex gap-4 mb-2">
+							<label class="flex items-center">
+								<input
+									type="radio"
+									name="allergies"
+									value="no"
+									bind:group={allergies}
+									class="mr-2"
+								/>
+								Ne
+							</label>
+							<label class="flex items-center">
+								<input
+									type="radio"
+									name="allergies"
+									value="yes"
+									bind:group={allergies}
+									class="mr-2"
+								/>
+								Ano
+							</label>
+						</div>
+						{#if allergies === "yes"}
+							<div class="flex flex-col">
+            <textarea
+							name="allergiesDescription"
+							bind:value={allergiesDescription}
+							maxlength="300"
+							placeholder="Popište vaše alergie (max 300 znaků)"
+							class="w-full px-4 py-2 text-base bg-white border border-gray-300 rounded-lg shadow-sm appearance-none text-gray-placeholder-gray-400 focus:outline-none focus:border-green-600"
+							rows="3"
+						></textarea>
+								<span class="text-sm text-gray-500 mt-1">
+                Zbývá {300 - (allergiesDescription?.length || 0)} znaků
+            </span>
+							</div>
+						{/if}
+					</div>
+
+					<!-- Způsob dodání -->
+					<div class="mt-6 mb-4">
+						<h3 class="text-lg font-medium mb-2">Způsob dodání</h3>
+						<div class="flex flex-col gap-2">
+							<label class="flex items-center">
+								<input
+									type="radio"
+									name="deliveryMethod"
+									value="own"
+									bind:group={deliveryMethod}
+									class="mr-2"
+								/>
+								Vlastní nosič
+							</label>
+							<label class="flex items-center">
+								<input
+									type="radio"
+									name="deliveryMethod"
+									value="reBox"
+									bind:group={deliveryMethod}
+									class="mr-2"
+								/>
+								REkrabička (záloha 160 Kč za set/80 Kč za jednu)
+							</label>
+							<label class="flex items-center">
+								<input
+									type="radio"
+									name="deliveryMethod"
+									value="menuBox"
+									bind:group={deliveryMethod}
+									class="mr-2"
+								/>
+								Menu Box (12 Kč/kus)
+							</label>
+						</div>
+					</div>
+
+					<!-- Způsob platby -->
+					<div class="mt-6 mb-4">
+						<h3 class="text-lg font-medium mb-2">Způsob platby</h3>
+						<div class="flex flex-col gap-2">
+							<label class="flex items-center">
+								<input
+									type="radio"
+									name="paymentMethod"
+									value="cash"
+									bind:group={paymentMethod}
+									class="mr-2"
+								/>
+								Hotově
+							</label>
+							<label class="flex items-center">
+								<input
+									type="radio"
+									name="paymentMethod"
+									value="bankNoInvoice"
+									bind:group={paymentMethod}
+									class="mr-2"
+								/>
+								Na účet bez faktury
+							</label>
+							<label class="flex items-center">
+								<input
+									type="radio"
+									name="paymentMethod"
+									value="bankWithInvoice"
+									bind:group={paymentMethod}
+									class="mr-2"
+								/>
+								Na účet s fakturou
+							</label>
+						</div>
+					</div>
+
+
+					<!--display-->
 					<div class="flex w-full my-4">
 						<button
 							type="submit"
