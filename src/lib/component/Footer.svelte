f781267228370428721141c567410ff8fbb2e005
mikigroup <39709130+mikigroup@users.noreply.github.com>
2025-03-25 11:32:32 +0100

    uprava footeru - doplneni ob. podminek + malicko jiny design

--- src/lib/component/Footer.svelte
+++ src/lib/component/Footer.svelte
@@ -1,23 +1,23 @@
 <footer class="border rounded-lg mt-40 border-gray-300">
 	<div class="p-4 text-gray-500 w-full">
-		<div class="flex justify-between items-center">
-			<div class="text-sm">
-				<p>
-					Vytvořeno s
-					<a
-					class="items-center text-sm"
-					target="_blank"
-					href="https://www.malyleo.cz/"
-					><i class="fa fa-regular fa-hand-spock" /> MalyLeo™
-					</a>
-					- core1.0b
-				</p>
-			</div>
+		<div class="flex justify-between items-center flex-col md:flex-row gap-2">
 			<div class="text-sm text-center hover:underline ">
 				<a href="/obchodni-podminky">Obchodní podmínky</a>
 			</div>
 			<div class="text-sm">
 				<p>Šťastné srdce s.r.o. 2022 - 2025</p>
+			</div>
+			<div class="text-sm">
+				<p>
+					Vytvořeno s
+					<a
+						class="items-center text-sm"
+						target="_blank"
+						href="https://www.malyleo.cz/"
+					><i class="fa fa-regular fa-hand-spock" /> MalyLeo™
+					</a>
+					- core1.0b
+				</p>
 			</div>
 		</div>
 	</div>
