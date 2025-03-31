c9cfe981b5bfcd4e366b0280d9fe1c408f72e8ea
mikigroup <mikigroup@gmail.com>
2025-03-31 12:32:01 +0200

    1

--- src/lib/component/Footer.svelte
+++ src/lib/component/Footer.svelte
@@ -1,19 +1,19 @@
 <footer class="bg-gradient-to-r from-gray-50 to-gray-100 shadow-inner pt-5">
 	<div class="max-w-7xl mx-auto px-4 py-6">
 		<div class="flex flex-col md:flex-row justify-between items-center gap-4">
-			<div class="flex space-x-6">
-				<a href="/obchodni-podminky" class="text-gray-600 hover:text-indigo-600 transition-colors duration-200 text-sm font-medium hover:underline">
+			<div class="flex flex-col md:flex-row items-center justify-center gap-2 md:gap-6 md:pb-0">
+				<a href="/obchodni-podminky" class="text-gray-600 hover:text-indigo-600 transition-colors duration-200 text-sm font-medium hover:underline text-center">
 					Obchodní podmínky
 				</a>
-				<a href="#" class="text-gray-600 hover:text-indigo-600 transition-colors duration-200 text-sm font-medium hover:underline">
+				<a href="#" class="text-gray-600 hover:text-indigo-600 transition-colors duration-200 text-sm font-medium hover:underline text-center">
 					Ochrana osobních údajů
 				</a>
-				<a href="#" class="text-gray-600 hover:text-indigo-600 transition-colors duration-200 text-sm font-medium hover:underline">
+				<a href="#" class="text-gray-600 hover:text-indigo-600 transition-colors duration-200 text-sm font-medium hover:underline text-center">
 					Kontakt
 				</a>
 			</div>
 
-			<div class="text-gray-500 text-sm">
+			<div class="text-gray-500 text-xs md:text-sm mt-5 md:mt-0">
 				<p>© Šťastné srdce s.r.o. 2022 - 2025. Všechna práva vyhrazena.</p>
 			</div>
 
