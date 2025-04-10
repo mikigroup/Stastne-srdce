c7432ff8a8ec6b97b2b713e3abc828ccfca994fd
mikigroup <mikigroup@gmail.com>
2025-04-10 21:48:20 +0200

    1

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,3 +1,7 @@
+import type { EmailOtpType } from "@supabase/supabase-js";
+import { redirect } from "@sveltejs/kit";
+import type { RequestHandler } from "./$types";
+
 export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
 	try {
 		// Podrobné logování všech parametrů
