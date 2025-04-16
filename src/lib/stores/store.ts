ee509edcf75011eb57e6a8dd1f78ff196d9bc6a9
mikigroup <39709130+mikigroup@users.noreply.github.com>
2025-04-16 15:06:21 +0200

    doplnen ROUTES do store.ts

--- src/lib/stores/store.ts
+++ src/lib/stores/store.ts
@@ -210,3 +210,34 @@ function createTotalPiecesStore() {
 }
 
 export const totalPiecesStore = createTotalPiecesStore();
+
+export const ROUTES = readable({
+	ADMIN: {
+		BASE: "/admin",
+		CUSTOMER: {
+			LIST: "/admin/customer",
+			NEW: "/admin/customer/newcustomer",
+			EDIT: (id: string) => `/admin/customer/${id}`
+		},
+		MENU: {
+			LIST: "/admin/menu",
+			NEW: "/admin/menu/newmenu",
+			EDIT: (id: string) => `/admin/menu/${id}`
+		},
+		ORDER: {
+			LIST: "/admin/order",
+			NEW: "/admin/order/neworder",
+			EDIT: (id: string) => `/admin/order/${id}`
+		},
+		SETTINGS: "/admin/settings"
+	},
+	CUSTOMER: {
+		HOME: "/",
+		MENU: "/obedy",
+		CART: "/kosik",
+		PROFILE: "/profile",
+		CONTACT: "/kontakt",
+		LOGIN: "/login",
+		SIGNUP: "/signup"
+	}
+} as const);
